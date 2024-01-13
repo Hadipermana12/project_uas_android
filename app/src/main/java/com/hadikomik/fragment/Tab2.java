f6111b8dca1eddb21bae5542df6031108c02f58e
Hadipermana12 <uciha.khadoetz10@gmail.com>
2024-01-13 11:37:50 +0700

    updaate

---
+++ app/src/main/java/com/hadikomik/fragment/Tab2.java
@@ -0,0 +1,42 @@
+package com.hadikomik.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.fragment.app.Fragment;
+import com.hadikomik.R;
+import com.hadikomik.sinopsis.sinopsis_dki;
+import com.hadikomik.sinopsis.sinopsis_msb;
+
+public class Tab2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate( R.layout.fragment_tab2, container, false );
+
+        ImageView imageView = view.findViewById(R.id.msb);
+        imageView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getActivity(), sinopsis_msb.class );
+                startActivity( intent );
+            }
+        });
+
+        ImageView imageView2 = view.findViewById(R.id.dki);
+        imageView2.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getActivity(), sinopsis_dki.class );
+                startActivity( intent );
+            }
+        });
+
+        return view;
+    }
+    private void OnToggleClicked() {
+    }
+}
---
+++ app/src/main/java/com/hadikomik/sinopsis/sinopsis_cz.java
@@ -0,0 +1,22 @@
+package com.hadikomik.sinopsis;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.hadikomik.R;
+
+public class sinopsis_cz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sinopsis_cz);
+
+        WebView webView1 = findViewById(R.id.webView1);
+        String video1 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/mc5X05k21IY\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        webView1.loadData(video1, "text/html", "utf-8");
+        webView1.getSettings().setJavaScriptEnabled(true);
+        webView1.setWebChromeClient(new WebChromeClient());
+    }
+}
